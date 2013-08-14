50851dfa809fc2f3699874c6a2f7983311000991
Brian Racer <bracer@gmail.com>
2013-08-14 13:07:57 -0600

    default to varnish 3

--- recipes/default.rb
+++ recipes/default.rb
@@ -31,7 +31,7 @@ if platform?("ubuntu", "debian")
   apt_repository "varnish-cache.org" do
     uri "http://repo.varnish-cache.org/#{node[:platform]}/"
     distribution node['lsb']['codename']
-    components ["main"]
+    components ["varnish-3.0"]
     key "http://repo.varnish-cache.org/debian/GPG-key.txt"
     deb_src true
     notifies :run, resources(:execute => "apt-get update"), :immediately
