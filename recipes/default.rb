4e69b78d35b5e36159de30485f4e67fbcf109c33
Brian Racer <bracer@gmail.com>
2013-08-14 13:07:44 -0600

    fix platform insertion

--- recipes/default.rb
+++ recipes/default.rb
@@ -29,7 +29,7 @@ end
 if platform?("ubuntu", "debian")
   include_recipe "apt"
   apt_repository "varnish-cache.org" do
-    uri "http://repo.varnish-cache.org/#{:platform}/"
+    uri "http://repo.varnish-cache.org/#{node[:platform]}/"
     distribution node['lsb']['codename']
     components ["main"]
     key "http://repo.varnish-cache.org/debian/GPG-key.txt"
